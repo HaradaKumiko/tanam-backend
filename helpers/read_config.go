146b2ec741cf29386cdc6a35912bf4b110a141d8
Farhan Rivaldy <fariv.fariv12@gmail.com>
2024-05-13 09:37:29 +0800

    deploy issue #1

--- helpers/read_config.go
+++ helpers/read_config.go
@@ -2,18 +2,11 @@ package helpers
 
 import (
 	"log"
-	"os"
 
 	"github.com/spf13/viper"
 )
 
 func GetConfig(key string) string {
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatalf("error getting current working directory: %s\n", err)
-	}
-	log.Printf("Current working directory: %s\n", dir)
-
 	viper.AddConfigPath(".")
 	viper.SetConfigFile(".env")
 
